df370c4ad1b48298b219a5c43fc9aff40d67b51b
tarkh <tarkhx@gmail.com>
2022-04-30 14:08:40 +0300

    add nemo-share, add terminator to nemo

---
+++ package/archw-tools/updates/1.1.19.sh
@@ -0,0 +1,15 @@
+#
+# Set as default terminal
+gsettings set org.gnome.desktop.default-applications.terminal exec terminator
+gsettings set org.cinnamon.desktop.default-applications.terminal exec terminator
+gsettings set org.cinnamon.desktop.default-applications.terminal exec-arg -e
+
+#
+# Nemo file-roller
+sudo pacman --noconfirm -S nemo-fileroller
+
+#
+# Nemo network share
+sudo pacman --noconfirm -S avahi nss-mdns nemo-share
+sudo systemctl enable avahi-daemon.service
+sudo systemctl start avahi-daemon.service
--- package/nemo/install.sh
+++ package/nemo/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman --noconfirm -S nemo
+sudo pacman --noconfirm -S nemo nemo-fileroller
 
 if [ "$S_SYSTEM_FM" == "nemo" ]; then
   xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
@@ -9,3 +9,8 @@ fi
 #
 # Do not display hidden files by default
 gsettings set org.nemo.preferences show-hidden-files false
+
+#
+# Network share
+sudo pacman --noconfirm -S avahi nss-mdns nemo-share
+sudo systemctl enable avahi-daemon.service
