ba7c9a1ea716cde9cf96b802b2eba01de5f62a69
tarkh <tarkhx@gmail.com>
2022-07-07 17:59:21 +0300

    Update install.sh

--- package/vscodium/install.sh
+++ package/vscodium/install.sh
@@ -6,8 +6,8 @@ yay --noconfirm -S vscodium-bin
 
 # Theme
 mkdir -p $V_HOME/.vscode-oss/extensions
-git clone https://github.com/dracula/visual-studio-code.git $V_HOME/.vscode/extensions/theme-dracula
-$V_HOME/.vscode/extensions/theme-dracula
+git clone https://github.com/dracula/visual-studio-code.git $V_HOME/.vscode-oss/extensions/theme-dracula
+$V_HOME/.vscode-oss/extensions/theme-dracula
 # Check for npm
 if ! npm -v > /dev/null 2>&1; then
   # Install NodeJS and NPM
