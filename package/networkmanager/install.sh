ced47d2721c6b4de7520c4f0c36e111b7526b1b0
tarkh <tarkhx@gmail.com>
2022-08-16 12:49:34 +0300

    Added l2tp to NM

--- package/networkmanager/install.sh
+++ package/networkmanager/install.sh
@@ -3,7 +3,9 @@
 sudo systemctl stop NetworkManager.service > /dev/null 2>&1
 service_ctl user off aw-nm-applet-autostart.service
 
-sudo pacman --noconfirm -S networkmanager network-manager-applet
+#
+# Install NetworkManager and add l2tp functionality
+sudo pacman --noconfirm -S networkmanager network-manager-applet networkmanager-l2tp strongswan
 
 #
 # Config
@@ -13,32 +15,9 @@ sudo pacman --noconfirm -S networkmanager network-manager-applet
 #dhcp=dhclient
 #EOL
 
-#
-# If IW connected
-if [ -f "${S_PKG}/autonetworkwifi" ]; then
-  #. ${S_PKG}/autonetworkwifi
-  #echo "Reconnecting WiFi with NetworkManager"
-  #INAME=$(iw dev | grep Interface | cut -d " " -f2)
-  #iwctl station $INAME disconnect
-  #iwctl known-networks "${AN_SSID}" forget
-  #sudo systemctl stop iwd.service
-  sudo systemctl disable iwd.service
-  sleep 2
-  sudo systemctl enable NetworkManager.service
-  #sudo systemctl start NetworkManager.service
-  #sleep 2
-  #sudo nmcli dev wifi
-  #echo "Connecting to wifi network ${AN_SSID}... This might take up to 60 seconds..."
-  #sleep 5
-  #nmcli device wifi connect "${AN_SSID}" password "$AN_PASS"
-  #sleep 2
-  #ip address show
-  #
-else
-  sudo systemctl disable iwd.service
-  sleep 2
-  sudo systemctl enable NetworkManager.service
-fi
+sudo systemctl disable iwd.service
+sleep 2
+sudo systemctl enable NetworkManager.service
 
 #
 # Enable IWD backend
