ae08a0ada1e907cf31ae2a5d4b5fa867c829fbd7
tarkh <tarkhx@gmail.com>
2023-03-10 18:11:37 +0300

    update theme installation

--- package/vscodium/install.sh
+++ package/vscodium/install.sh
@@ -5,17 +5,20 @@
 yay --noconfirm -S vscodium-bin
 
 # Theme
-mkdir -p $V_HOME/.vscode-oss/extensions
-git clone https://github.com/dracula/visual-studio-code.git $V_HOME/.vscode-oss/extensions/theme-dracula
-$V_HOME/.vscode-oss/extensions/theme-dracula
+#mkdir -p $V_HOME/.vscode-oss/extensions
+#git clone https://github.com/dracula/visual-studio-code.git $V_HOME/.vscode-oss/extensions/theme-dracula
+#$V_HOME/.vscode-oss/extensions/theme-dracula
 # Check for npm
-if ! npm -v > /dev/null 2>&1; then
+#if ! npm -v > /dev/null 2>&1; then
   # Install NodeJS and NPM
-  . $S_PKG/nodejs/install.sh
-fi
+#  . $S_PKG/nodejs/install.sh
+#fi
 # Duild theme
-npm install
-npm run build
+#npm install
+#npm run build
+
+vscodium --install-extension dracula-theme.theme-dracula
+
 # Create user settings
 mkdir -p $V_HOME/.config/VSCodium/User
 bash -c "cat > $V_HOME/.config/VSCodium/User/settings.json" << EOL
