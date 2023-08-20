cddc005e1531ec7378dc22af33809b208c3bb33c
tarkh <tarkhx@gmail.com>
2023-08-20 22:42:03 +0300

    git mtrack because of pr merge

--- package/mtrack/install.sh
+++ package/mtrack/install.sh
@@ -2,17 +2,17 @@
 
 #
 # Install from AUR
-#yay --noconfirm -S xf86-input-mtrack
+yay --noconfirm -S xf86-input-mtrack-git
 
 #
 # PATCHED version
 # Build and install from tarkh's GitHub
-cd $V_AUR
-curl -L ${V_TR}/xf86-input-mtrack/tarball/scrollCoastingEnhancement | tar -xz
-cd tarkh-xf86-input-mtrack*
-sudo pacman --noconfirm -S xorg-server-devel xorgproto pixman
-autoreconf --install && ./configure --prefix=/usr && make && sudo make install
-cd $S_PKG
+#cd $V_AUR
+#curl -L ${V_TR}/xf86-input-mtrack/tarball/scrollCoastingEnhancement | tar -xz
+#cd tarkh-xf86-input-mtrack*
+#sudo pacman --noconfirm -S xorg-server-devel xorgproto pixman
+#autoreconf --install && ./configure --prefix=/usr && make && sudo make install
+#cd $S_PKG
 
 #
 # Copy config
