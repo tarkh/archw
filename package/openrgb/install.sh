2cf9c28c325190b26cfa87e40a1228304e9d98f2
tarkh <tarkhx@gmail.com>
2025-02-10 14:13:15 +0300

    openrgb autoload profile

--- package/openrgb/install.sh
+++ package/openrgb/install.sh
@@ -5,4 +5,16 @@ yay --noconfirm -S openrgb-git
 #
 # Default ArchW conf
 mkdir -p $V_HOME/.config/OpenRGB/
-\cp ./package/openrgb/ArchW.orp $V_HOME/.config/OpenRGB/
+cp ./package/openrgb/ArchW.orp $V_HOME/.config/OpenRGB/
+
+#
+# Autorun with i3
+service_ctl user install-on ./package/openrgb/systemd/openrgb-load-profile.service
+
+#
+# On
+service_ctl user on openrgb-load-profile.service
+
+#
+# Copy openrgb load profile to systemd sleep
+sudo cp ./package/openrgb/systemd/openrgb-load-profile /usr/lib/systemd/system-sleep/
